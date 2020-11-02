c3fe07bce660bbeb2168026e97c505f8d62fe543
jzapata <marr0n-Blanc0n>
2020-11-02 01:37:43 -0500

    Configured "Material UI" and "Styled Components" to used together.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,29 +1,47 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
-import { StylesProvider } from '@material-ui/styles';
+import React, {Component} from "react";
+import {render} from "react-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import styled, {ThemeProvider} from 'styled-components';
 import Button from "@material-ui/core/Button";
+import theme from './theme';
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+const StyledButton = styled(Button)`
+  color: ${props => props.theme.palette.primary.main};
+`;
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      loaded: false,
-      placeholder: "Loading"
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            loaded: false,
+            placeholder: "Loading"
+        };
+    }
 
-  render() {
-    return (
-      <StylesProvider injectFirst>
-        <Button> I am a Button </Button>
-      </StylesProvider>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Container maxWidth="sm">
+                    <Box my={4}>
+                        <Typography variant="h4" component="h1" gutterBottom>
+                            Create React App v4-beta example
+                        </Typography>
+                        <StyledButton>Styled Button</StyledButton>
+                        <Button>Button</Button>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default App;
 
 const container = document.getElementById("app");
-render(<App />, container);
+render(<App/>, container);
