25ca88db304f10808be447f4b4f54163d1512944
jzapata <marr0n-Blanc0n>
2020-11-06 04:52:29 -0500

    Migrate App class to hooks

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Guides Followed:
 
 Material UI and Styled Components
 - https://levelup.gitconnected.com/material-ui-styled-components-fff4d345fb07 
+- https://github.com/mui-org/material-ui/tree/master/examples/create-react-app
+- https://material-ui.com/styles/basics/#installation
+
+
+
+TODO: Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.
+ https://codesandbox.io/s/github/mui-org/material-ui/tree/master/examples/create-react-app?file=/src/index.js
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
 import styled, {ThemeProvider} from 'styled-components';
 import Button from "@material-ui/core/Button";
@@ -10,38 +10,30 @@ import Typography from "@material-ui/core/Typography";
 
 const StyledButton = styled(Button)`
   color: ${props => props.theme.palette.primary.main};
+  background: black
 `;
 
-class App extends Component {
+const App = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            loaded: false,
-            placeholder: "Loading"
-        };
-    }
+    const [data, setData] = useState([]);
 
-    render() {
-        return (
-            <ThemeProvider theme={theme}>
-                <CssBaseline/>
-                <Container maxWidth="sm">
-                    <Box my={4}>
-                        <Typography variant="h4" component="h1" gutterBottom>
-                            Create React App v4-beta example
-                        </Typography>
-                        <StyledButton>Styled Button</StyledButton>
-                        <Button>Button</Button>
-                    </Box>
-                </Container>
-            </ThemeProvider>
-        );
-    }
-}
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Container maxWidth="sm">
+                <Box my={4}>
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Create React App v4-beta example
+                    </Typography>
+                    <StyledButton>Styled Button (Edited).</StyledButton>
+                    <Button>Button (No edited).</Button>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
+};
 
 export default App;
 
 const container = document.getElementById("app");
-render(<App/>, container);
+ReactDOM.render(<App/>, container);
