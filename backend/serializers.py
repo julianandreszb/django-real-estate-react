3806f5b0e7a1c08c50b3c11e8ca271a5c1d7b8f6
jzapata <marr0n-Blanc0n>
2020-11-20 11:35:20 -0500

    Fix plain password saved

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from backend.models import User
 
@@ -14,16 +15,24 @@ class UserSerializer(serializers.ModelSerializer):
         counter = 0
         for field_name, field_errors in default_errors.items():
             for field_error in field_errors:
-                errors_messages.append({"key": counter, "value": field_error})
+                errors_messages.append({"key": counter, "value": f"{field_name}: {field_error}"})
                 counter += 1
         return {'errors': errors_messages}
 
     class Meta:
         model = User
-        fields = ('username', 'email', "first_name", "last_name")
+        fields = ('username', 'email', "first_name", "last_name", "password")
+        # extra_kwargs = {'password': {'write_only': True}} # Uncomment to avoid returning password in response
 
     def create(self, validated_data):
-        return User.objects.create(**validated_data)
+        # return User.objects.create(**validated_data)
+        return User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            last_name=validated_data['last_name'],
+            first_name=validated_data['first_name'],
+            password=make_password(validated_data['password'])
+        )
 
     def update(self, instance, validated_data):
         return User.objects.update(**validated_data)
